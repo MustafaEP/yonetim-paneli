a8463b3a9abcf424de8d469797a8e8c4c5decc8b
MustafaEP <portakalm12@gmail.com>
2025-12-29 19:27:27 +0300

    sadas

--- backend/prisma/migrations/20250130000001_update_dealer_to_contracted_institution/migration.sql
+++ backend/prisma/migrations/20250130000001_update_dealer_to_contracted_institution/migration.sql
@@ -1,10 +1,5 @@
 -- Update MemberSource enum: DEALER -> CONTRACTED_INSTITUTION
--- First, add the new enum value
-ALTER TYPE "MemberSource" ADD VALUE IF NOT EXISTS 'CONTRACTED_INSTITUTION';
-
--- Update existing data from DEALER to CONTRACTED_INSTITUTION
-UPDATE "Member" SET "source" = 'CONTRACTED_INSTITUTION' WHERE "source" = 'DEALER';
-
--- Note: We cannot remove the old DEALER value from enum in PostgreSQL
--- It will remain in the enum but won't be used
+-- This migration is marked as applied in docker-entrypoint.sh
+-- Because PostgreSQL enum additions must be in a separate transaction
+-- Manual steps will be executed if needed
 
